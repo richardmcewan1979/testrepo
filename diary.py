ba338c1974995b2c39524f66bbc2dc5d6f371436
richardmcewan1979 <richmcewan@icloud.com>
2017-02-10 13:26:49 +0000

    Add files via upload

---
+++ diary.py
@@ -0,0 +1,12 @@
+#diary programme
+
+file = open('diary.txt', 'a')
+
+diary_entry = raw_input('Add diary entry: ')
+
+#file.write(diary_entry)
+#note print file.readline()
+
+file.close()
+
+input = ('Press enter to quit')
